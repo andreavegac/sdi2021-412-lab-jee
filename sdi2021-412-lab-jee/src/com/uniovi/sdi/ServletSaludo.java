cc675b60dd6dfdfc17ce208e35ac57a8f8a760ac
andreavegac <andrea.gjn@gmail.com>
2021-02-05 21:33:24 +0100

    SDI-2021-412-1.1-Los Servlets son multihilo

---
+++ sdi2021-412-lab-jee/src/com/uniovi/sdi/ServletSaludo.java
@@ -0,0 +1,64 @@
+package com.uniovi.sdi;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletSaludo
+ */
+@WebServlet("/ServletSaludo")
+public class ServletSaludo extends HttpServlet implements Servlet {
+	private static final long serialVersionUID = 1L;
+	private int counter = 0;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletSaludo() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.print("<HTML>");
+		out.print("<HEAD><TITLE>Hola Mundo!</TITLE></HEAD>");
+		out.print("<BODY>");
+		String nombre = (String) request.getParameter("nombre");
+		if(nombre!=null) {
+			out.println("Hola " + nombre + "<br>");
+		}
+		
+		try {
+			Thread.sleep(15000);
+		}catch (InterruptedException e) {
+			//do nothing 
+		}
+		out.println("ID del hilo: " + Thread.currentThread().getId() + "<br>");
+		counter++;
+		out.println("Visitas: " + counter + "<br>");
+		out.print("</BODY></HTML>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
